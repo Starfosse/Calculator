67e1a1e2ef2c51ffeb973711a32d81fa71d3e2e9
Starfosse <alexis.bouleau92@yahoo.fr>
2023-07-05 17:11:30 +0200

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,1 +1,32 @@
 //alert("Bonjour, je suis un fichier JavaScript lié à votre page HTML !");
+
+
+const screen = document.querySelector("#screen");
+const listNumberButton = document.querySelectorAll(".number");
+const operatorButton = document.querySelectorAll(".operator");
+const backButton = document.querySelector("#back");
+const cButton = document.querySelector("#C");
+
+for(let button of listNumberButton)
+{
+    button.addEventListener("click", () =>
+    {
+        if(screen.textContent == 0)
+            screen.textContent = button.value;
+    })
+}
+
+function operatorHandler(nbr1, nbr2, operator)
+{
+    switch(operator)
+    {
+        case "÷":
+            return(nbr1 / nbr2);
+        case "x":
+            return(nbr1 * nbr2);
+        case "-":
+            return(nbr1 - nbr2);
+        case "+":
+            return(nbr1 + nbr2);
+    }
+}
