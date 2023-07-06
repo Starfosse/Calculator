376e5ce1a6586aa98ad69c71782e4a13d678cb08
Starfosse <alexis.bouleau92@yahoo.fr>
2023-07-07 00:09:59 +0200

    js - done

--- script/script.js
+++ script/script.js
@@ -1,32 +1,78 @@
 //alert("Bonjour, je suis un fichier JavaScript lié à votre page HTML !");
 
-
 const screen = document.querySelector("#screen");
-const listNumberButton = document.querySelectorAll(".number");
-const operatorButton = document.querySelectorAll(".operator");
-const backButton = document.querySelector("#back");
-const cButton = document.querySelector("#C");
+const listButton = document.querySelectorAll("button");
+let memoryOperator = null;
+let memoryNumber = null;
+let newScreen = null;
 
-for(let button of listNumberButton)
+for(let button of listButton)
 {
-    button.addEventListener("click", () =>
-    {
-        if(screen.textContent == 0)
-            screen.textContent = button.value;
-    })
+    if (!isNaN(button.value))
+        button.addEventListener("click", numberHandler);
+    else
+        button.addEventListener("click", operatorHandler);
 }
 
-function operatorHandler(nbr1, nbr2, operator)
+function numberHandler()
 {
-    switch(operator)
+    if(newScreen)
+    {
+        screen.textContent = 0;
+        newScreen = null;
+    }
+    screen.textContent = Number(screen.textContent) * 10 + Number(this.value);
+}
+
+function operatorHandler()
+{
+    switch(this.value)
     {
         case "÷":
-            return(nbr1 / nbr2);
+            memoryNumber = screen.textContent;
+            memoryOperator = "÷";
+            newScreen = 1;
+            break;
         case "x":
-            return(nbr1 * nbr2);
+            memoryNumber = screen.textContent;
+            memoryOperator = "x";
+            newScreen = 1;
+            break;
         case "-":
-            return(nbr1 - nbr2);
+            memoryNumber = screen.textContent;
+            memoryOperator = "-";
+            newScreen = 1;
+            break;
         case "+":
-            return(nbr1 + nbr2);
+            memoryNumber = screen.textContent;
+            memoryOperator = "+";
+            newScreen = 1;
+            break;
+        case "=":
+            screen.textContent = getResult();
+            memoryNumber = screen.textContent;
+            newScreen = 1;
+            break;
+        case "C":
+            screen.textContent = 0;
+            break;
+        case "←":
+            if(screen.textContent.length > 1)
+                screen.textContent = screen.textContent.substring(0,screen.textContent.length - 1);
+            else
+                screen.textContent = 0;
+            break;
     }
+}
+
+function getResult()
+{
+    if (memoryOperator == "÷")
+        return (memoryOperator = null, Number(memoryNumber) / Number(screen.textContent));
+    else if (memoryOperator == "x")
+        return (memoryOperator = null, Number(memoryNumber) * Number(screen.textContent));
+    else if (memoryOperator == "-")
+        return (memoryOperator = null, Number(memoryNumber) - Number(screen.textContent));
+    else if (memoryOperator == "+")
+        return (memoryOperator = null, Number(memoryNumber) + Number(screen.textContent));
 }
