45a9d9d4d770cd17d7e75c58eeaf0be11d7bba0a
Starfosse <alexis.bouleau92@yahoo.fr>
2023-07-09 18:04:26 +0200

    update stateManager

--- script/script.js
+++ script/script.js
@@ -1,78 +1,118 @@
 //alert("Bonjour, je suis un fichier JavaScript lié à votre page HTML !");
+// gsap
+// const screen = document.querySelector("#screen");
 
-const screen = document.querySelector("#screen");
-const listButton = document.querySelectorAll("button");
-let memoryOperator = null;
-let memoryNumber = null;
-let newScreen = null;
 
-for(let button of listButton)
-{
-    if (!isNaN(button.value))
-        button.addEventListener("click", numberHandler);
-    else
-        button.addEventListener("click", operatorHandler);
+function stateManager() {
+    let stateCalculator = {
+        memoryOperator : null,
+        memoryNumber : null,
+        newScreen : null,
+        screen : document.querySelector("#screen")
+    };
+
+    function copyObject() {
+        return { ...stateCalculator };
+    }
+
+    function updateState(updater) {
+        const newState = typeof updater === 'function' ? updater(copyObject()) : updater;
+        if (newState) {
+            stateCalculator = newState;
+        }
+    }
+
+    function getState() {
+        return copyObject();
+    }
+
+    return {
+        updateState,
+        getState,
+    }
+}
+
+const { updateState, getState } = stateManager();
+
+init();
+
+function init() {
+    const listButton = document.querySelectorAll("button");
+    listButton.forEach(button => {
+        !isNaN(button.value)
+            ? button.addEventListener("click", numberHandler)
+            : button.addEventListener("click", operatorHandler);
+    })
 }
 
 function numberHandler()
 {
-    if(newScreen)
-    {
-        screen.textContent = 0;
-        newScreen = null;
-    }
-    screen.textContent = Number(screen.textContent) * 10 + Number(this.value);
+    updateState(state => {
+        if(state.newScreen)
+        {
+            state.screen.textContent = 0;
+            state.newScreen = null;
+        }
+        state.screen.textContent = Number(state.screen.textContent) * 10 + Number(this.value);
+        return state;
+    });
 }
 
 function operatorHandler()
 {
-    switch(this.value)
-    {
-        case "÷":
-            memoryNumber = screen.textContent;
-            memoryOperator = "÷";
-            newScreen = 1;
-            break;
-        case "x":
-            memoryNumber = screen.textContent;
-            memoryOperator = "x";
-            newScreen = 1;
-            break;
-        case "-":
-            memoryNumber = screen.textContent;
-            memoryOperator = "-";
-            newScreen = 1;
-            break;
-        case "+":
-            memoryNumber = screen.textContent;
-            memoryOperator = "+";
-            newScreen = 1;
-            break;
-        case "=":
-            screen.textContent = getResult();
-            memoryNumber = screen.textContent;
-            newScreen = 1;
-            break;
-        case "C":
-            screen.textContent = 0;
-            break;
-        case "←":
-            if(screen.textContent.length > 1)
-                screen.textContent = screen.textContent.substring(0,screen.textContent.length - 1);
-            else
-                screen.textContent = 0;
-            break;
-    }
+    updateState(state => {
+        switch(this.value)
+        {
+            case "÷":
+                state.memoryNumber = state.screen.textContent;
+                state.memoryOperator = "÷";
+                state.newScreen = 1;
+                break;
+            case "x":
+                state.memoryNumber = state.screen.textContent;
+                state.memoryOperator = "x";
+                state.newScreen = 1;
+                break;
+            case "-":
+                state.memoryNumber = state.screen.textContent;
+                state.memoryOperator = "-";
+                state.newScreen = 1;
+                break;
+            case "+":
+                state.memoryNumber = state.screen.textContent;
+                state.memoryOperator = "+";
+                state.newScreen = 1;
+                break;
+            case "=":
+                state.screen.textContent = getResult();
+                state.memoryNumber = state.screen.textContent;
+                state.newScreen = 1;
+                break;
+            case "C":
+                state.screen.textContent = 0;
+                break;
+            case "←":
+                if(state.screen.textContent.length > 1)
+                state.screen.textContent = state.screen.textContent.substring(0,state.screen.textContent.length - 1);
+                else
+                    state.screen.textContent = 0;
+                break;
+        }
+        return state;
+    });
 }
 
 function getResult()
 {
-    if (memoryOperator == "÷")
-        return (memoryOperator = null, Number(memoryNumber) / Number(screen.textContent));
-    else if (memoryOperator == "x")
-        return (memoryOperator = null, Number(memoryNumber) * Number(screen.textContent));
-    else if (memoryOperator == "-")
-        return (memoryOperator = null, Number(memoryNumber) - Number(screen.textContent));
-    else if (memoryOperator == "+")
-        return (memoryOperator = null, Number(memoryNumber) + Number(screen.textContent));
+    updateState(state => {
+    if (state.memoryOperator == "÷")
+        return (state.memoryOperator = null, Number(state.memoryNumber) / Number(state.screen.textContent));
+    else if (state.memoryOperator == "x")
+        return (state.memoryOperator = null, Number(state.memoryNumber) * Number(state.screen.textContent));
+    else if (state.memoryOperator == "-")
+        return (state.memoryOperator = null, Number(state.memoryNumber) - Number(state.screen.textContent));
+    else if (state.memoryOperator == "+")
+        return (state.memoryOperator = null, Number(state.memoryNumber) + Number(state.screen.textContent));
+    return state;
+    })
 }
