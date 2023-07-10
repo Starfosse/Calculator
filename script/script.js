637f1f91ccef4c0618f986c168fa3e3a0f15cbff
Starfosse <alexis.bouleau92@yahoo.fr>
2023-07-10 16:34:31 +0200

    fix

--- script/script.js
+++ script/script.js
@@ -6,7 +6,8 @@
 function stateManager() {
     let stateCalculator = {
         memoryOperator : null,
-        memoryNumber : null,
+        memoryOldNbr : 0,
+        memoryNewNbr : 0,
         newScreen : null,
         screen : document.querySelector("#screen")
     };
@@ -47,13 +48,15 @@ function init() {
 
 function numberHandler()
 {
+    const buttonValue = this.value;
     updateState(state => {
         if(state.newScreen)
         {
             state.screen.textContent = 0;
             state.newScreen = null;
         }
-        state.screen.textContent = Number(state.screen.textContent) * 10 + Number(this.value);
+        state.memoryNewNbr = buttonValue;
+        state.screen.textContent = Number(state.screen.textContent) * 10 + Number(buttonValue);
         return state;
     });
 }
@@ -64,38 +67,49 @@ function operatorHandler()
         switch(this.value)
         {
             case "÷":
-                state.memoryNumber = state.screen.textContent;
+                if(state.newScreen != 1)
+                    getResult();
+                state.memoryOldNbr = state.screen.textContent;
                 state.memoryOperator = "÷";
                 state.newScreen = 1;
                 break;
             case "x":
-                state.memoryNumber = state.screen.textContent;
+                if(state.newScreen != 1)
+                    getResult();
+                state.memoryOldNbr = state.screen.textContent;
                 state.memoryOperator = "x";
                 state.newScreen = 1;
                 break;
             case "-":
-                state.memoryNumber = state.screen.textContent;
+                if(state.newScreen != 1)
+                    getResult();
+                state.memoryOldNbr = state.screen.textContent;
                 state.memoryOperator = "-";
                 state.newScreen = 1;
                 break;
             case "+":
-                state.memoryNumber = state.screen.textContent;
+                if(state.newScreen != 1)
+                    getResult();
+                state.memoryOldNbr = state.screen.textContent;
                 state.memoryOperator = "+";
                 state.newScreen = 1;
                 break;
             case "=":
-                state.screen.textContent = getResult();
-                state.memoryNumber = state.screen.textContent;
+                state.newScreen === 1
+                    ? getResultRecursive()
+                    : getResult();
+                state.memoryOldNbr = state.screen.textContent;
                 state.newScreen = 1;
                 break;
             case "C":
                 state.screen.textContent = 0;
+                state.memoryOldNbr = null;
+                state.memoryOperator = null;
                 break;
             case "←":
-                if(state.screen.textContent.length > 1)
-                state.screen.textContent = state.screen.textContent.substring(0,state.screen.textContent.length - 1);
-                else
-                    state.screen.textContent = 0;
+                state.screen.textContent.length > 1
+                    ? state.screen.textContent = state.screen.textContent.substring(0,state.screen.textContent.length - 1)
+                    : state.screen.textContent = 0;
                 break;
         }
         return state;
@@ -106,13 +120,28 @@ function getResult()
 {
     updateState(state => {
     if (state.memoryOperator == "÷")
-        return (state.memoryOperator = null, Number(state.memoryNumber) / Number(state.screen.textContent));
+        state.screen.textContent = Number(state.memoryOldNbr) / Number(state.screen.textContent);
     else if (state.memoryOperator == "x")
-        return (state.memoryOperator = null, Number(state.memoryNumber) * Number(state.screen.textContent));
+        state.screen.textContent = Number(state.memoryOldNbr) * Number(state.screen.textContent);
     else if (state.memoryOperator == "-")
-        return (state.memoryOperator = null, Number(state.memoryNumber) - Number(state.screen.textContent));
+        state.screen.textContent = Number(state.memoryOldNbr) - Number(state.screen.textContent);
     else if (state.memoryOperator == "+")
-        return (state.memoryOperator = null, Number(state.memoryNumber) + Number(state.screen.textContent));
+        state.screen.textContent = Number(state.memoryOldNbr) + Number(state.screen.textContent);
+    return state;
+    })
+}
+
+function getResultRecursive()
+{
+    updateState(state => {
+    if (state.memoryOperator == "÷")
+        state.screen.textContent = Number(state.screen.textContent) / Number(state.memoryNewNbr);
+    else if (state.memoryOperator == "x")
+        state.screen.textContent = Number(state.screen.textContent) * Number(state.memoryNewNbr);
+    else if (state.memoryOperator == "-")
+        state.screen.textContent = Number(state.screen.textContent) - Number(state.memoryNewNbr);
+    else if (state.memoryOperator == "+")
+        state.screen.textContent = Number(state.screen.textContent) + Number(state.memoryNewNbr);
     return state;
     })
 }
